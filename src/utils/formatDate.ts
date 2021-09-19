cfed9c5c15808717e0f1cfe7057ee903a9ccdeb3
lucasdzuc <lucas_morais_@hotmail.com>
2021-09-19 19:16:44 -0300

    modified hook formatDate

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,14 +1,15 @@
-import { format, parseISO } from 'date-fns'
+import { format, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt-BR';
 
 export default function formatDate(date: string){
 
   const firstDate = parseISO(date);
 
-  const formattedDate = format(firstDate, "dd/MM/yyyy' às 'HH:mm'");
+  // const formattedDate = format(firstDate, "dd/MM/yyyy' às 'HH:mm'");
   
-  // const formattedDate = format(firstDate, "dd 'de' MMMM',  às 'HH:mm'h'");
-  // const znDate = zonedTimeToUtc(formattedDate, 'America/Sao_Paulo');
-  //' dd/MM/yyyy' + ' HH:mm:ss'
+  const formattedDate = format(firstDate, "dd 'de' MMMM',  às 'HH:mm", {
+    locale: pt,
+  });
 
   return formattedDate;
 }
